06b49127e318f4742ff228676d1724c627f6b8ec
Colleen Geohagan <ckbowker@gmail.com>
2018-05-26 22:49:04 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp      = require('gulp'),
+    sass     = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    cleancss    = require('gulp-clean-css'),
+    eslint  = require('gulp-eslint'),
+    pump = require('pump'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify');
+
+gulp.task('styles', function(){
+    return gulp.src('scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(cleancss())
+        .pipe(sourcemaps.write(''))
+        .pipe(gulp.dest(''))        
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('assets/development/js/*.js'),
+        uglify(),
+        gulp.dest('assets/js')
+    ],
+    cb
+  );
+});
+
+gulp.task('lint', () => {
+    // ESLint ignores files with "node_modules" paths. 
+    // So, it's best to have gulp ignore the directory as well. 
+    // Also, Be sure to return the stream from the task; 
+    // Otherwise, the task may end before the stream has finished. 
+    return gulp.src(['assets/**/*.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property 
+        // of the file object so it can be used by other modules. 
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console. 
+        // Alternatively use eslint.formatEach() (see Docs). 
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on 
+        // lint error, return the stream and pipe to failAfterError last. 
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('default',['styles', 'compress']);
+
+gulp.task('watch', function() {  
+    // Watch .scss files
+    gulp.watch('scss/*.scss', ['styles']);
+    gulp.watch('scss/**/*.scss', ['styles']); 
+
+    // Watch .js files
+    gulp.watch('assets/development/js/*.js', ['compress']);
+});
+
